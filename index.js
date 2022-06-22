2856e116b9df2ed06f7a43c41852e30558fbdbcd
MAshar <ashar@claryicon.com>
2022-06-22 19:33:40 +0500

    Handled DPI for x and y point.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var tryRequire = require('try-require')
 var $ = require('dombo')
 
-var electron = tryRequire('electron')
-var remote = electron ? electron.remote : tryRequire('remote')
+var remote = tryRequire('@electron/remote')
 
 var mouseConstructor = tryRequire('osx-mouse') || tryRequire('win-mouse')
 
 var supported = !!mouseConstructor
 var noop = function () { return noop }
+var windowWidth = 0
+var windowHeight = 0
 
-var drag = function (element) {
+var drag = function (element, width, height) {
   element = $(element)
+  windowWidth = width
+  windowHeight = height
 
   var offset = null
   var mouse = mouseConstructor()
@@ -24,15 +27,17 @@ var drag = function (element) {
   mouse.on('left-drag', function (x, y) {
     if (!offset) return
 
-    x = Math.round(x - offset[0])
-    y = Math.round(y - offset[1])
+    const pos = remote.screen.getCursorScreenPoint()
+    x = Math.round(pos.x - offset[0])
+    y = Math.round(pos.y - offset[1])
 
-    // setPosition throws error if called with -0
-    remote.getCurrentWindow().setPosition(x + 0, y + 0)
+    remote.getCurrentWindow().setBounds({width: windowWidth, height: windowHeight, x: x, y: y})
   })
 
   mouse.on('left-up', function () {
     offset = null
+    windowWidth = 0
+    windowHeight = 0
   })
 
   return function () {
